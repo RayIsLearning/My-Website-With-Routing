cc4b21ed22ce39e93fbb61ffd0742adc99da8321
Raymond Cooke <64932993+RayIsLearning@users.noreply.github.com>
2025-02-06 16:02:27 -0500

    Update route.js

--- route.js
+++ route.js
@@ -3,16 +3,16 @@
         if (url === '/') {//HOME
             document.getElementById('content').innerHTML =`
             <div id="softwareButton" class="left">Software <button id="softwareButton">software</button><br>
-                <img src="/bin.png" alt="Software" id="software">
+                <img src="bin.png" alt="Software" id="software">
             </div>
 
             <div class="middle">Media / Social / Art<br><br><br>
                 <div class="tooltiptext">In Progress</div>
-                <img src="/string.jpg" alt="Swirly Graphic">
+                <img src="string.jpg" alt="Swirly Graphic">
             </div> 
 
             <div id="aboutButton" class="right">About me <button id="aboutButton">About</button><br>
-                <img src="/Xmas.jpg" alt="Pic of me">
+                <img src="Xmas.jpg" alt="Pic of me">
             </div>
 
             <div class="footer">
@@ -37,13 +37,13 @@
                 </a>
             </div>  
             <div class="right">My Resume <br><br>
-                <a href="/rezzy.pdf" target="_blank">
+                <a href="rezzy.pdf" target="_blank">
                     <img src="factory.jpg" width="480" height="480">
                 </a>
 
             </div>
             <div class="footer">
-                <img src="/home.png"alt="Home Icon" id="home">
+                <img src="home.png"alt="Home Icon" id="home">
                 <h3>All About Ray</h3>
                 <p>Jesus Saves &nbsp;</p>
                 <a href="https://usccb.cld.bz/Catechism-of-the-Catholic-Church/27/" target="_blank">
@@ -64,11 +64,11 @@
             
             <div class="middle">Roman Numeral Converter<br><br>
                 <a href="roman.html" target="_blank">
-                    <img src="/bustslil.jpg" alt="An example image">
+                    <img src="bustslil.jpg" alt="An example image">
                 </a>
             </div>  
             <div class="right">Roll the dice! <br><br><br>
-                <img src="/dice.gif" alt="Dice Game">
+                <img src="dice.gif" alt="Dice Game">
             </div>
             <!-- TODO FIX THE HOME ICON -->
             <div class="footer">
